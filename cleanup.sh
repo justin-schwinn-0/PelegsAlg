c89bce719cba878b3f87b1dff190e826b97de7fe
justin-schwinn-0 <justinsch1029@gmail.com>
2024-06-23 22:56:27 -0500

    fix clean up

--- cleanup.sh
+++ cleanup.sh
@@ -22,7 +22,7 @@ cat $CONFIGLOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
         host=$( echo $line | awk '{ print $2 }' )
 
         echo $host
-        gnome-terminal -e -- "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host killall -u $netid" &
+        gnome-terminal -- "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host killall -u $netid" &
         sleep 1
 
         n=$(( n + 1 ))
