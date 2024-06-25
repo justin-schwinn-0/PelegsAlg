9e3c430fca51332f913520a61e56715c32ba97a9
justin-schwinn-0 <justinsch1029@gmail.com>
2024-06-24 20:47:08 -0500

    making progress

--- main.cpp
+++ main.cpp
@@ -180,7 +180,7 @@ void acceptMsgs(Node& n)
 
 void tryConnections(Node& n)
 {
-    using namespace std::chrono_literals;
+    using std::chrono_literals;
 
     while(!n.isConnectedToNeighbors())
     {
