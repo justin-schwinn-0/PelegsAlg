659026fc37869b1afc696821c90ec0919dae5de4
justin-schwinn-0 <justinsch1029@gmail.com>
2024-06-25 00:21:19 -0500

    making progress

--- Node.cpp
+++ Node.cpp
@@ -70,7 +70,8 @@ void Node::acceptNeighbors()
 
     std::cout << "\n\nwaiting for connection..." << std:: endl;
     struct sockaddr socketAddress;
-    int rxFd = accept(mListenFd,&socketAddress,sizeof(socketAddress));
+    int addrLength;
+    int rxFd = accept(mListenFd,&socketAddress,&addrLength);
 
     if(rxFd < 0)
     {
