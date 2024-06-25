6a91d72640e66578b51a1c47baa1800e7fddd87b
justin-schwinn-0 <justinsch1029@gmail.com>
2024-06-25 00:04:46 -0500

    making progress

--- Node.cpp
+++ Node.cpp
@@ -56,6 +56,10 @@ void Node::connectNeighbors()
 {
     while(!outConnectedToNeighbors())
     {
+        for(auto& n : mNeighbors)
+        {
+            n.outGoingConnect();
+        }
     }
 }
 
