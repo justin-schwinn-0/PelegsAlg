0c71de2a01c4e53e15d5a2a9d4aaee6f2ebbacf2
justin-schwinn-0 <justinsch1029@gmail.com>
2024-06-25 22:28:12 -0500

    lets make these messages work

--- Connection.h
+++ Connection.h
@@ -27,7 +27,7 @@ public:
 
     void outGoingConnect();
 
-    void setConnection(int fd,sockaddr* farEnd);
+    void setConnection(int fd,sockaddr farEnd);
 
     void msgTx(std::string msg);
     void msgRx();
@@ -46,7 +46,7 @@ private:
     std::string hostname;
     uint32_t port;
 
-    struct sockaddr* mFarAddress;
+    struct sockaddr mFarAddress;
 
     int mConFd = -1;
 };
