3033d10fbba69ea040d6c14f0f6d247214933fe0
justin-schwinn-0 <justinsch1029@gmail.com>
2024-06-26 22:10:38 -0500

    messages work, onto the algorithms

--- Sync.cpp
+++ Sync.cpp
@@ -2,7 +2,7 @@
 #include "Node.h"
 #include "Utils.h"
 
-Sync::Sync(int neighbors, Node& n) : rNode(n), mRound(0)
+Sync::Sync(int neighbors, Node& n) : rNode(n), mRound(0), affixedVectors(false)
 {
     mHasRecvd = std::vector<std::pair<int,bool>>(neighbors,std::pair<int,bool>(-1,false));
     payloadCache = std::vector<std::pair<int,std::string>>(neighbors,std::pair<int,std::string>(-1,""));
