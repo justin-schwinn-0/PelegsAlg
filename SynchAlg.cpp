ea6f054bb41dc666aae443721b2e9beba086052e
justin-schwinn-0 <justinsch1029@gmail.com>
2024-06-27 17:02:06 -0500

    messages work, onto the algorithms

--- SynchAlg.cpp
+++ SynchAlg.cpp
@@ -8,5 +8,7 @@ SynchAlg::SynchAlg(Node& n) :
 
 std::string SynchAlg::wrapPayload(std::string payload,int round)
 {
+    
+    Utils::log("wrapping payload {",payload,"} ",round);
     return std::to_string(rNode.getUid()) + "::" + std::to_string(round) + "::" + payload;
 }
