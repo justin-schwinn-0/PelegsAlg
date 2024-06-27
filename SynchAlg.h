702f710cdfbcf30f20f6b6a3567de74a985deda2
justin-schwinn-0 <justinsch1029@gmail.com>
2024-06-27 16:33:31 -0500

    messages work, onto the algorithms

--- SynchAlg.h
+++ SynchAlg.h
@@ -9,13 +9,14 @@ class SynchAlg
 public:
     SynchAlg(Node& n); 
 
-    std::string wrapPayload(std::string payload,int round);
-
     virtual void handlePayload(std::string str) = 0;
 
     virtual void proceedRound(int round) = 0;
 
 protected:
+
+    std::string wrapPayload(std::string payload,int round);
+
     Node& rNode;
 };
 
