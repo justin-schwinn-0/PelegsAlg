03a40891f2179eda635565892a2919a8d8f935c6
justin-schwinn-0 <justinsch1029@gmail.com>
2024-06-27 19:25:27 -0500

    messages work, onto the algorithms

--- PelegsAlg.h
+++ PelegsAlg.h
@@ -8,11 +8,18 @@
 class PelegsAlg : public SynchAlg
 {
 public:
-    TestAlg(Node& n);
+    PelegsAlg(Node& n);
 
     void handlePayload(std::string payload);
 
     void proceedRound(int round);
+
+private:
+    int knownHighest;
+    int dist;
+    int distMax;
+
+    int roundsSinceChange;
 
 };
 
